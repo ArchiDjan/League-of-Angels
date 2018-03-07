f79f81199dc84f453dec784cd323140e66c7d0da
ArchiDjan <wander579000@gmail.com>
2018-03-07 03:41:02 +0300

    added functional slide paging

--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,64 @@
 $(function() {
-	var slides = document.getElementsByClassName('slider')[0].children;
-	var numSlides = slides.length;
-	var count = 0;
+	var initialPos, currentWidth,																																	// позиция и ширина слайда
+	slides 												= document.getElementsByClassName('slider')[0].children,				// количество слайдов
+  numSlides 										= slides.length - 1,																						 
+	timeDelay 										= 100,		                                                      // длительность смены слайда
+	btnChars 											= document.getElementsByClassName('toggleChar')[0].children,    // коллекция кнопок персонажей
+	numIcon 											= 0,                                                            // порядковый номер кнопки персонажа из коллекции
+	maxNumIcon 										= btnChars.length - 1;
 
-	$('#arrowL').bind('click', moveLeft);
-	var initialPos;
 
-	function moveLeft() {
-		initialPos = $('.slider').css('right').replace('px', '')*1;
-		var newPos = (initialPos == 0) ? 0 : initialPos - 1920;
-		alert(newPos);
-		$('.slider').animate({left: newPos}, 500);
+
+	$('.toggleChar li:first').addClass('active');
+
+	// обработчик при нажатии на иконку персонажа
+	for (var i = 0; i < btnChars.length; i++) {
+		btnChars[i].addEventListener('click', function() {
+			$('.toggleChar li').removeClass('active');
+			$(this).addClass("active");
+			toggleSlide();
+		});
 	}
 
-	$('#arrowR').bind('click', moveRight);	
+/*	function toggleSlide(event) {
+		var currentPos = event.currentTarget;
+		alert(currentPos);
+	}*/
 
+	// обработчик при нажатии на стрелку влево
+	$('#arrowL').bind({'click': moveLeft, 'mousedown': switchIconL});																			
+
+	// сдвигает контейнер со слайдами на одну позицию влево
+	function moveLeft() {	
+		initialPos = $('.slider').css('left').replace('px', '')*1;
+		currentWidth = $('.slider').css('width').replace('px', '')*1;
+		var newPos = (initialPos == 0) ? 0 : initialPos + currentWidth;
+		$('.slider').animate({left: newPos}, timeDelay);
+	}
+
+	// меняет иконки персонажей при листании слайдов
+	function switchIconL() {
+		numIcon = (numIcon == 0) ? 0 : --numIcon;
+		$('.toggleChar li').removeClass('active');
+		btnChars[numIcon].className = btnChars[numIcon].className + " " + "active";
+	}
+
+	// обработчик при нажатии на стрелку вправо
+	$('#arrowR').bind({'click': moveRight, 'mousedown': switchIconR});	
+
+	// сдвигает контейнер со слайдами на одну позицию вправо
 	function moveRight() {
-		initialPos = $('.slider').css('right').replace('px', '')*1;
-		var newPos = (initialPos >= 1920*2) ? initialPos : initialPos + 1920;
-		alert(newPos);
-		$('.slider').animate({right: newPos}, 500);
+		initialPos = $('.slider').css('left').replace('px', '')*1;
+		currentWidth = $('.slider').css('width').replace('px', '')*1;
+		var newPos = (initialPos <= -currentWidth*numSlides) ? initialPos : initialPos - currentWidth;
+		$('.slider').animate({left: newPos}, timeDelay);
+	}
+
+	// меняет иконки персонажей при листании слайдов
+	function switchIconR() {
+		numIcon = (numIcon < maxNumIcon) ? ++numIcon : maxNumIcon;
+		$('.toggleChar li').removeClass('active');
+		btnChars[numIcon].className = btnChars[numIcon].className + " " + "active";
 	}
 
 });
-
-
-
-
-
-/*var nodes = document.getElementsByClassName('grid')[0].children;
-
-for (var i = 0; i < nodes.length; i++) {
-	var item = nodes[i];
-	 
-	var handl = function() {
-		var a = ++i;
-		return function() {
-			alert(a);
-		}
-	}
-	item.onclick = handl();
-}*/
